8dddaed8692cf1ea586f36e808ee4a034021b416
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-01-31 16:31:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -32,7 +32,7 @@ cat << EOF > /usr/local/etc/xray/config.json
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/app/DxHsYsOjTsGwZk"
+          "path": "$WS"
         }
       }
     }
