50a6b8fe3322e76b48100c3ae36783d9a9623ccd
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-02-21 15:21:13 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,7 +9,7 @@ install -m 755 /tmp/xray/geosite.dat /usr/local/bin/geosite.dat
 install -m 755 /tmp/xray/geoip.dat /usr/local/bin/geoip.dat
 
 xray -version
- 
+
 # Remove temporary directory
 rm -rf /tmp/xray
 
@@ -23,36 +23,25 @@ cat << EOF > /usr/local/etc/xray/config.json
   "inbounds": [
     {
       "port": $PORT,
-      "protocol": "vless",
+      "protocol": "VLESS",
       "settings": {
         "clients": [
           {
             "id": "$UUID",
-            "alterId": 0,
-            "level": 0,
-            "email": "vless@in.com"
+            "alterId": 0
           }
         ],
         "decryption": "none"
       },
       "streamSettings": {
-      //  "network": "tcp"
-      
-      //  "network": "grpc",
-      //  "grpcSettings": {
-      //    "serviceName": "$VL" 
-          
-                "network": "ws",
-                "security": "none",
-                "allowInsecure": false,
-                "wsSettings": {            
-                    "path": "$VL" 
-                }   
-                
-        }   
-     
-    } 
-    
+//        "network": "tcp"
+        "network": "ws",
+        "allowInsecure": false,      
+        "wsSettings": {
+          "path": "$VL"
+        }
+      }
+    }
     
   ],
   
@@ -63,22 +52,14 @@ cat << EOF > /usr/local/etc/xray/config.json
       "domain": [
         "geosite:category-ads-all"
       ],
-      "outboundTag": "block"
+      "outboundTag": "blocked"
     },
-      {
-        "type": "field",
-        "ip": [
-          "geoip:private"
-        ],
-        "outboundTag": "block"
-      },    
-    
     {
       "type": "field",
       "domain": [
         "geosite:cn"
       ],
-      "outboundTag": "allow"
+      "outboundTag": "directly"
     }
   ]
 },
@@ -86,40 +67,16 @@ cat << EOF > /usr/local/etc/xray/config.json
   "outbounds": [
     {
       "protocol": "freedom",
-      "tag":"allow"
+      "tag":"directly"
     },
         {
             "protocol": "blackhole",
-            "tag": "block"
+            "tag": "blocked"
         }
   ]
 }
 EOF
 
-cat << EOF > /Kaddyfile
-
-http://localhost:8088 {
-
-   reverse_proxy $VL* http://127.0.0.1:443
-
-    reverse_proxy * http://www.anneleephotography.com {    
-        header_up Host {www.anneleephotography.com}    
-        }
-
-}
-
-EOF
-
-
-# Run xray caddy
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json  &
-
-#caddy run 
-
-#caddy run --config /Kaddyfile --adapter caddyfile
-
-
-netstat
-
-
+# Run xray
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json & sleep 10 & netstat 
 
