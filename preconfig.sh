04cc111cf3dda9327f0ddcaccb83e6a1ea579b43
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-04-03 15:42:28 +0800

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -183,6 +183,9 @@ until /app/tailscale up --authkey=${AUTH} --hostname=${HOST} --advertise-exit-no
 do 
     sleep 1  
 done 
+echo Tailscale started
+ALL_PROXY=socks5://localhost:1059/
+
 
 else
 
