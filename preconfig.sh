be8d68bc228a757ecdbd571d02637e4c39177164
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-04-03 14:55:53 +0800

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -177,18 +177,18 @@ EOF
 
 # Run tailscale 
 if $TAILSCALE = "true"; then
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1059 & 
+
+/usr/local/bin/ssray -config /usr/local/etc/ssray/config.json && /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1059 & 
 until /app/tailscale up --authkey=${AUTH} --hostname=${HOST} --advertise-exit-node 
 do 
     sleep 1  
 done 
+
+else
+
+/usr/local/bin/ssray -config /usr/local/etc/ssray/config.json
+
 fi
 
-
-# Run ssray
-until /usr/local/bin/ssray -config /usr/local/etc/ssray/config.json 
-do 
-    sleep 1  
-done 
 
 
